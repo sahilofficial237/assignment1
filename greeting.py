ad67e2ef5a5c8b68411e81936d3483d176f7d38c
sahilofficial237 <sahilofficial237@gmail.com>
2025-09-25 16:54:05 +0530

    Add files via upload

---
+++ greeting.py
@@ -0,0 +1,6 @@
+#taking inputs from the user
+name=input("enter the first name:")
+surname=input("enter the last name:")
+
+#greeting code
+print(f" hello, {name} {surname}! welcome to the python program.")
---
+++ mathmatical_operations.py
@@ -0,0 +1,16 @@
+#program for performing mathmatical operations 
+
+#taking the inputs
+num1=float(input("enter the first number:"))
+num2=float(input("enter the second number:"))
+
+#operation code
+add=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+
+print("addition:",add)
+print("subtraction:",sub)
+print("multiplication:",mul)
+print("divison:",div)
